f4b1a9ed8d1277da93578248749f0c2c758b7557
Jordan Moss <jmoss636@gmail.com>
2020-07-14 19:19:29 +0100

    code cleanup

--- pocs/oauth-server/passport/index.js
+++ pocs/oauth-server/passport/index.js
@@ -7,7 +7,6 @@ passport.use(new GoogleStrategy({
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: '/auth/google/redirect'
 }, (accessToken, refreshToken, profile, done) => {
-    //check if user already exists in our db with the given profile id
     GoogleUser.findOne({googleId: profile.id}).then((currentUser) => {
         if(currentUser){
             done(null, currentUser)
@@ -22,13 +21,11 @@ passport.use(new GoogleStrategy({
 }))
 
 passport.serializeUser((user, done) => {
-    console.log('serializeUser: ' + user._id)
     done(null, user.id)
 })
 
 passport.deserializeUser((id, done) => {
     GoogleUser.findById(id).then(user => {
-        console.log('Deserializer', user)
         done(null, user)
     })
 })
