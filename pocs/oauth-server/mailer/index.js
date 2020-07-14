b0ac5bdfc6ec191116c00d9c3673e9abc7aa22e7
Jordan Moss <jmoss636@gmail.com>
2020-07-14 19:57:11 +0100

    fix sendMail

--- pocs/oauth-server/mailer/index.js
+++ pocs/oauth-server/mailer/index.js
@@ -12,8 +12,8 @@ function createMailOptions(to, subject, text) {
 function sendMail(to, subject, text) {
     if(!mailOptions.to) console.error('Invalid mailOptions')
     transporter.sendMail(createMailOptions(to, subject, text), (err, info) => {
-        if(err) console.log(err)
-        console.log('Email sent:', info.response)
+        if(err) return console.log(err)
+        return console.log('Email sent:', info.response)
     })
 }
 
