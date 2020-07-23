7008605228a94349644c701dc72e0de9619cb0a7
michaelKurowski <Byamarro@gmail.com>
2020-07-23 18:24:49 +0200

    created src dir with server and client

---
+++ src/client/src/router/index.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Registration from '../views/Registration.vue'
+import Profile from '../views/Profile.vue'
+import Tree from '../views/Tree.vue'
+import EditMode from '../views/EditMode.vue'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/registration',
+    name: 'Registration',
+    component: Registration
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile
+  },
+  {
+    path: '/tree',
+    name: 'Tree',
+    component: Tree
+  },
+  {
+    path: '/edit-mode',
+    name: 'EditMode',
+    component: EditMode
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
