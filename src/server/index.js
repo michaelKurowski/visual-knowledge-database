55ce1aa78412c473866d1e1cf4fd47159b11dffb
michaelKurowski <Byamarro@gmail.com>
2020-08-09 03:45:30 +0300

    enabled cors

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = require('./router')
 const bodyParser = require('body-parser')
-
+const cors = require('cors')
 
 const PORT = 8000
 
 
 
 const app = express()
+app.use(cors())
 app.use(bodyParser.json())
 app.use(router)
 app.listen(PORT, () => {
